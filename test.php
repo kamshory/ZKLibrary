4d14316556f9862adf33dbe15aafccd377842be0
Kamshory <kamshory@yahoo.com>
2016-03-28 16:32:31 +0700

    Added files via upload

---
+++ test.php
@@ -0,0 +1,49 @@
+<?php
+include "zklibrary.php";
+$zk = new ZKLibrary('192.168.1.102', 4370);
+$zk->connect();
+$zk->disableDevice();
+
+$users = $zk->getUser();
+?>
+<table width="100%" border="0" cellspacing="0" cellpadding="0">
+<thead>
+  <tr>
+    <td width="25">No</td>
+    <td>UID</td>
+    <td>ID</td>
+    <td>Name</td>
+    <td>Role</td>
+    <td>Password</td>
+  </tr>
+</thead>
+
+<tbody>
+<?php
+$no = 0;
+foreach($users as $key=>$user)
+{
+  $no++;
+?>
+
+  <tr>
+    <td align="right"><?php echo $no;?></td>
+    <td><?php echo $key;?></td>
+    <td><?php echo $user[0];?></td>
+    <td><?php echo $user[1];?></td>
+    <td><?php echo $user[2];?></td>
+    <td><?php echo $user[3];?></td>
+  </tr>
+
+<?php
+}
+?>
+
+</tbody>
+</table>
+<?php
+
+$zk->enableDevice();
+$zk->disconnect();
+
+?>
